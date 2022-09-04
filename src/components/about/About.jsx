750be6f4b24df1a36b7e9ac61bafb83e15588efd
Daniel Soto Delgado <d.soto@gmx.at>
2022-09-04 19:23:02 +0200

    scrollbar smoothed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
           <h1 className="aboutme text-center">Introduction:</h1>
           <p className="text-justify">
               I´m a Front End Developer with a creative attitude to technical problem solving and a good sense for design.
-              I have about one year experience and I want to make connections and to expand on to bigger projects.
+              I have one year experience and I want to make connections to expand on to bigger projects.
           </p>            
           <div className="w-100 flex">
               <p className="inline-block pb-12">I built this site using </p>
