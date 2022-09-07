2283766a79c2faf7067f0e263d82764b0f620f5b
Daniel Soto Delgado <d.sotodelgado@outlook.com>
2022-09-07 15:33:55 +0200

    rewrote about text

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./About";
 import "./about.css";
-import FREEDSD from "../../assets/Danielfree22.png";
+// import FREEDSD from "../../assets/Danielfree22.png";
 import "./About.js";
 
 const About = () => {
@@ -9,12 +9,20 @@ const About = () => {
   return (
     <section id="about" className='flex'>
       <div className="flex-col-reverse m-auto xl:flex xl:flex-row-reverse xl:w-2/3  ">        
-        <img className='rounded-full center w-40 h-40 mx-auto xl:w-56 xl:h-56 ' src={FREEDSD} alt="looking nice"></img>
+        {/* <img className='rounded-full center w-40 h-40 mx-auto xl:w-56 xl:h-56 ' src={FREEDSD} alt="looking nice"></img> */}
         <div className="p-5 ">
-          <h1 className="aboutme text-center">Introduction:</h1>
+          <h1 className="aboutme text-center">About me:</h1>
           <p className="text-justify">
-              I´m a Front End Developer with a creative attitude to technical problem solving and a good sense for design.
-              I have one year experience and I want to make connections to expand on to bigger projects.
+
+              I started working as a sound engineer in my early twenties.
+              Through mixing, recording and composing music I learned not to give up easily when faced with technical challenges and to communicate effectively.
+              When working as a sound operator on a cruise ship through Norway I fell in love with the natural beauty of the country, 
+              but decided to go back to Austria to take a position as a project manager for amusement park attractions.
+              I was responsible for planning, scheduling and execution of a whole portfolio of innovative multimedia attractions.
+              Besides the hardware we developed controller software, interactive multimedia games and computer animations I got curious 
+              about how the visual realisation was done practically. 
+              So I started my Front End journey and found a real passion in bringing design ideas on to the screen.
+              Now I want to expand my practical knowledge to real life projects to make the internet a better experience for everybody.
           </p>            
           <div className="w-100 flex">
               <p className="inline-block pb-12">I built this site using </p>
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,12 +3,14 @@ import "./contact.css";
 import { MdOutlineEmail } from "react-icons/md";
 import { BsWhatsapp } from "react-icons/bs";
 import {BsLinkedin} from 'react-icons/bs'
+import FREEDSD from "../../assets/Danielfree22.png";
 
 
 const Contact = () => {
     return (
         <section id="contact">
             <h5>Get in touch</h5>
+            <img className='rounded-full center w-40 h-40 mx-auto xl:w-56 xl:h-56 ' src={FREEDSD} alt="looking nice"></img>
             <h2> Contact me</h2>
 
             <div className=" contact_container flex-col items-center">
@@ -24,7 +26,7 @@ const Contact = () => {
 
                 <div >
                     <article className="contact_option">
-                        <a href="https://api.whatsapp.com/?phone+436509513571" target="_blank" 
+                        <a href="https://api.whatsapp.com/?phone+436509513571" target="_blank" rel="noreferrer" 
                         className="flex">
                             <BsWhatsapp class="contactSymbol" />
                             <h4 className='symboltext'>WhatsApp</h4>
@@ -34,7 +36,7 @@ const Contact = () => {
 
                 <div className=''>
                     <article className="contact_option">
-                        <a href='https://www.linkedin.com/in/daniel-soto-delgado-21368866/' target='_blank' 
+                        <a href='https://www.linkedin.com/in/daniel-soto-delgado-21368866/' target='_blank' rel="noreferrer"  
                         className="flex">
                             <BsLinkedin className='contactSymbol'/>
                             <h4  className='symboltext'>LinkedIn</h4>
