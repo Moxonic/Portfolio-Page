66debebdc0d817613114da88cf04363fa021b7f8
Daniel Soto Delgado <d.sotodelgado@outlook.com>
2022-10-06 12:21:48 +0200

    refitted video containers

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -7,7 +7,7 @@ const Experience = () => {
 			<div className="h-screen grid grid-cols-1 content-center">
 				<h1 className=" text-center">Experience</h1>
 				<p className="legend text-center p-9">
-					In the following I present you some of the projects I worked on in the
+					In the following you will find some of the projects I worked on in the
 					last years.
 				</p>
 				<h5 className="p-9 text-center">Lean back and enjoy!</h5>
@@ -17,32 +17,34 @@ const Experience = () => {
 				<div className="
 				videoContainer
 				p-2 mx-3 my-9 flex-col rounded-md 
-				xl:flex xl:flex-row xl:m-28
-				xl:pb-5 xl:items-baseline 
+				xl:flex xl:flex-row xl:m-16 xl:items-center 
 				xl:justify-evenly
+				xl:gap-4 
 				">
-					<div className="videoWrapper  
-					xl:flex-row xl:pb-0  xl:w-1/2
-					">
-						<iframe
-							className="h-full w-full xl:h-96 "
-							frameborder="0"
-							scrolling="no"
-							allow="fullscreen;"
-							type="text/html"
-							src="https://www.youtube.com/embed/6ApGMwgCt_w?autoplay=0&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&vq=hd720&origin=https://youtubeembedcode.com"
-						></iframe>
+					<div className="wrapperwrapper">
+						<div className="videoWrapper  
+						xl:flex-row 
+						">
+							<iframe
+								className="h-full w-full xl:h-96 "
+								frameborder="0"
+								scrolling="no"
+								allow="fullscreen;"
+								type="text/html"
+								src="https://www.youtube.com/embed/6ApGMwgCt_w?autoplay=0&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&vq=hd720&origin=https://youtubeembedcode.com"
+							></iframe>
+						</div>
 					</div>
-					<div className="experience__description flex-col content-center pb-5 items-center xl:w-96 ">
-						<h3 className="pt-5">Eye to Ear - iPad App</h3>
+					<div className="experience__description flex-col content-center items-center xl:w-96 ">
+						<h3 className="pt-5 xl:pt-0">Eye to Ear - iPad App</h3>
 						<p className="text-justify">
 							Eye to Ear facilitates visually impaired museum visitors to
 							experience visual art through an interactive sonification
 							approach 
 						</p>
-						<h4 className="pt-5">Occupation: </h4>
+						<h4 className="pt-5 xl:pt-0">Occupation: </h4>
 						<p >Sound Art Direction & Sound Design</p>
-						<h4 className="pt-5">Awards: </h4>
+						<h4 className="pt-5  xl:pt-0">Awards: </h4>
 						<ul className=" text-center">
 							<li>Art Directors Club of Europe</li>
 							<li>Red Dot Junior Award 2017</li>
@@ -58,20 +60,22 @@ const Experience = () => {
 				<div className="
 				videoContainer
 				p-2 mx-3 my-9 flex-col rounded-md 
-				xl:flex xl:flex-row xl:m-28
-				xl:pb-5 xl:items-baseline 
-				xl:justify-evenly">
-					<div className="videoWrapper  
-					xl:flex-row xl:pb-0  xl:w-1/2
-					">
-						<iframe
-							className="h-full w-full xl:h-96 "
-							title="vimeo-player"
-							src="https://player.vimeo.com/video/127020709?h=b8366ff9e9"
-							frameborder="0"
-							allow="fullscreen;"></iframe>
+				xl:flex xl:flex-row xl:m-16 xl:items-center 
+				xl:justify-evenly
+				xl:gap-4">
+					<div className="wrapperwrapper">
+						<div className="videoWrapper  
+						xl:flex-row 
+						">
+							<iframe
+								className="h-full w-full xl:h-96 "
+								title="vimeo-player"
+								src="https://player.vimeo.com/video/127020709?h=b8366ff9e9"
+								frameborder="0"
+								allow="fullscreen;"></iframe>
+						</div>
 					</div>
-					<div className="experience__description flex-col content-center pb-5 items-center xl:w-96 ">
+					<div className="experience__description flex-col content-center items-center xl:w-96 ">
 						<h3 className="pt-5">Mobiglobe</h3>
 						<p className="text-justify">
 							Located adjacent to the Volkswagen headquarters in Wolfsburg,
@@ -82,7 +86,7 @@ const Experience = () => {
 						<h4 className="pt-5">Occupation: </h4>
 						<p className="">Sound Design</p>
 						<h4 className="pt-5">Awards: </h4>
-						<p className=" text-center xl:pb-14">Red Dot Award Communicationdesign</p>
+						<p className=" text-center ">Red Dot Award Communicationdesign</p>
 					</div>
 				</div>
 
@@ -90,23 +94,25 @@ const Experience = () => {
 				<div className="
 				videoContainer
 				p-2 mx-3 my-9 flex-col rounded-md 
-				xl:flex xl:flex-row xl:m-28
-				xl:pb-5 xl:items-baseline 
-				xl:justify-evenly">
+				xl:flex xl:flex-row xl:m-16 xl:items-center 
+				xl:justify-evenly
+				xl:gap-4">
 					
 					{/* //VIDEOS */}
-					<div className="videoWrapper  
-					xl:flex-row xl:pb-0  xl:w-1/2
-					">
-						<iframe
-							className='h-full w-full xl:h-96 '
-							title="vimeo-player"
-							src="https://player.vimeo.com/video/126129405?h=dbfddfd934"
-							frameborder="0"
-							allow="fullscreen;"
-						></iframe>
+					<div className="wrapperwrapper">
+						<div className="videoWrapper  
+						xl:flex-row 
+						">
+							<iframe
+								className='h-full w-full xl:h-96 '
+								title="vimeo-player"
+								src="https://player.vimeo.com/video/126129405?h=dbfddfd934"
+								frameborder="0"
+								allow="fullscreen;"
+							></iframe>
+						</div>
 					</div>
-					<div className="experience__description flex-col content-center pb-5 items-center xl:w-96">
+					<div className="experience__description flex-col content-center items-center xl:w-96">
 						<h3  className="pt-5">Sasso San Gottardo</h3>
 						<p className="text-justify">
 							Deep inside the swiss alps hides the once top secret Gotthard
@@ -115,33 +121,35 @@ const Experience = () => {
 							water, internet security and climate change
 						</p>
 						<h4 className="pt-5"> Occupation:</h4>
-						<p className="xl:pb-24"> Sound Design, Technical Installation</p>
+						<p > Sound Design, Technical Installation</p>
 					</div>
 				</div>
 
 				<div className="
 				videoContainer
 				p-2 mx-3 my-9 flex-col rounded-md 
-				xl:flex xl:flex-row xl:m-28
-				xl:pb-5 xl:items-baseline 
-				xl:justify-evenly">
+				xl:flex xl:flex-row xl:m-16 xl:items-center 
+				xl:justify-evenly
+				xl:gap-4">
 					
 					{/* //VIDEOS */}
-					<div className="videoWrapper  
-					xl:flex-row xl:pb-0  xl:w-1/2
-					">
-						<iframe
-							className='h-full w-full xl:h-96 '
-							frameborder="0"
-							allow="fullscreen;"
-							scrolling="no"
-							marginheight="0"
-							marginwidth="0"
-							width="788.54"
-							height="443"
-							type="text/html"
-							src="https://www.youtube.com/embed/IbSHel9CRT0?autoplay=0&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&vq=hd720&origin=https://youtubeembedcode.com"
-						></iframe>
+					<div className="wrapperwrapper">
+						<div className="videoWrapper  
+						xl:flex-row 
+						">
+							<iframe
+								className='h-full w-full xl:h-96 '
+								frameborder="0"
+								allow="fullscreen;"
+								scrolling="no"
+								marginheight="0"
+								marginwidth="0"
+								width="788.54"
+								height="443"
+								type="text/html"
+								src="https://www.youtube.com/embed/IbSHel9CRT0?autoplay=0&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&vq=hd720&origin=https://youtubeembedcode.com"
+							></iframe>
+						</div>
 					</div>
 					<div className="experience__description flex-col content-center pb-5 items-center xl:w-96">
 						<h3 className="pt-5">
@@ -153,33 +161,35 @@ const Experience = () => {
 						<h4 className="pt-5">Occupation: </h4>
 						<p className="">Project Management</p>
 						<h4>Awards: </h4>
-						<p className="xl:pb-36">Global Creative Technology Design Award</p>
+						<p className="">Global Creative Technology Design Award</p>
 					</div>
 				</div>
 
 				<div className="
 				videoContainer
 				p-2 mx-3 my-9 flex-col rounded-md 
-				xl:flex xl:flex-row xl:m-28
-				xl:pb-5 xl:items-baseline 
-				xl:justify-evenly">
+				xl:flex xl:flex-row xl:m-16 xl:items-center 
+				xl:justify-evenly
+				xl:gap-4">
 					
 					{/* //VIDEOS */}
-					<div className="videoWrapper  
-					xl:flex-row xl:pb-0  xl:w-1/2
-					">
-						<iframe
-							className='h-full w-full xl:h-96 '
-							frameborder="0"
-							allow="fullscreen;"
-							scrolling="no"
-							marginheight="0"
-							marginwidth="0"
-							width="788.54"
-							height="443"
-							type="text/html"
-							src="https://www.youtube.com/embed/8eZfYiCGXQc?autoplay=0&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&vq=hd720&origin=https://youtubeembedcode.com"
-							allowfullscreen></iframe>
+					<div className="wrapperwrapper">
+						<div className="videoWrapper  
+						xl:flex-row 
+						">
+							<iframe
+								className='h-full w-full xl:h-96 '
+								frameborder="0"
+								allow="fullscreen;"
+								scrolling="no"
+								marginheight="0"
+								marginwidth="0"
+								width="788.54"
+								height="443"
+								type="text/html"
+								src="https://www.youtube.com/embed/8eZfYiCGXQc?autoplay=0&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&vq=hd720&origin=https://youtubeembedcode.com"
+								allowfullscreen></iframe>
+						</div>
 					</div>
 					<div className="experience__description flex-col content-center pb-5 items-center xl:w-96">
 						<h3 className="pt-5 ">Attraktion! Showcase 2019/20</h3>
@@ -188,7 +198,7 @@ const Experience = () => {
 						dome attractions, interactive games and exhibits 
 						</p>
 						<h4 className="pt-5">Occupation:</h4>
-						<p className="xl:pb-44"> Project Management </p>
+						<p className=""> Project Management </p>
 					</div>
 				</div>
 			</div>
