4322896b146740c8e16144a4647925a0232a12e6
Daniel Soto Delgado <d.sotodelgado@outlook.com>
2022-10-07 21:04:45 +0200

    Animated sections

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,37 +4,43 @@ import "./about.css";
 // import FREEDSD from "../../assets/Danielfree22.png";
 import "./About.js";
 
+import { AnimationOnScroll } from 'react-animation-on-scroll';
+
 const About = () => {
 
   return (
     <section id="about" className='flex'>
-      <div className="flex-col-reverse m-auto xl:flex xl:flex-row-reverse xl:w-2/3  ">        
-        {/* <img className='rounded-full center w-40 h-40 mx-auto xl:w-56 xl:h-56 ' src={FREEDSD} alt="looking nice"></img> */}
-        <div className="p-5 ">
-          <h1 className="aboutme text-center">About me:</h1>
-          <p className="">
-
-              I started working as a sound engineer in my early twenties.
-              Through mixing, recording and composing music I learned 
-              not to give up easily when faced with technical challenges and to communicate effectively.
-              When working as a sound operator on a cruise ship through Norway 
-              I fell in love with the natural beauty of the country, 
-              but decided to go back to Austria to take a position as a project manager 
-              for amusement park attractions.
-              <br></br><br></br>
-              I was responsible for planning, scheduling and execution of a whole portfolio of innovative multimedia attractions.
-              Besides the hardware we developed controller software, interactive multimedia games and computer animations I got curious 
-              about how the visual realisation was done practically. 
-              <br></br><br></br>
-              So I started my Front End journey and found a real passion in bringing design ideas on to the screen.
-              Now I want to expand my practical knowledge to real life projects to make the internet a better experience for everybody from my new homebase in Oslo.
-          </p>            
-          <div className="w-100 flex">
-              <p className="inline-block pb-12">I built this site using </p>
-              <div className="">
-                <div className="word text-sky-400 text-2xl"></div>
+      <div className="flex-col-reverse m-auto xl:flex xl:flex-row-reverse xl:w-2/3">        
+        <div className="p-5">
+          <AnimationOnScroll animateIn="animate__fadeInUpBig" animateOnce="true">
+            <h1 className="aboutme text-center pb-4">About me:</h1>
+          </AnimationOnScroll>
+          <AnimationOnScroll animateIn="animate__fadeInUpBig" animateOnce="true">
+            <p className='pb-4'>
+                I started working as a sound engineer in my early twenties.
+                Through mixing, recording and composing music I learned 
+                not to give up easily when faced with technical challenges and to communicate effectively.
+                When working as a sound operator on a cruise ship through Norway 
+                I fell in love with the natural beauty of the country, 
+                but decided to go back to Austria to take a position as a project manager 
+                for amusement park attractions.
+            </p>
+            <p className='pb-4'>
+                I was responsible for planning, scheduling and execution of a whole portfolio of innovative multimedia attractions.
+                Besides the hardware we developed controller software, interactive multimedia games and computer animations I got curious 
+                about how the visual realisation was done practically. 
+            </p>
+            <p className='pb-4'>
+                So I started my Front End journey and found a real passion in bringing design ideas on to the screen.
+                Now I want to expand my practical knowledge to real life projects to make the internet a better experience for everybody from my new homebase in Oslo.
+            </p>         
+              <div className="w-100 flex">
+                  <p className="inline-block pb-12">I built this site using </p>
+                  <div className="">
+                    <div className="word text-sky-400 text-2xl"></div>
+                  </div>
               </div>
-          </div>
+            </AnimationOnScroll>  
         </div>
       </div>
     </section>
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,206 +1,211 @@
 import React from "react";
 import "./experience.css";
+import { AnimationOnScroll } from 'react-animation-on-scroll';
+
 
 const Experience = () => {
 	return (
 		<section id="experience">
 			<div className="h-screen grid grid-cols-1 content-center">
 				<h1 className=" text-center">Experience</h1>
-				<p className="legend text-center p-9">
-					In the following you will find some of the projects I worked on in the
-					last years.
-				</p>
-				<h5 className="p-9 text-center">Lean back and enjoy!</h5>
+				<AnimationOnScroll animateIn="animate__fadeIn" animateOnce="true">
+					<p className="legend text-center p-9">
+						In the following you will find some of the projects I worked on in the
+						last years.
+					</p>
+					<h5 className="p-9 text-center">Lean back and enjoy!</h5>
+				</AnimationOnScroll>
 			</div>
-
+			<AnimationOnScroll animateIn="animate__slideInLeft" animateOnce="true">
 			<div className="allvids">
-				<div className="
-				videoContainer
-				p-2 mx-3 my-9 flex-col rounded-md 
-				xl:flex xl:flex-row xl:m-16 xl:items-center 
-				xl:justify-evenly
-				 
-				">
-					<div className="wrapperwrapper xl:w-3/4 ">
-						<div className="videoWrapper  
-						xl:flex-row 
-						">
-							<iframe
-								className="h-full w-full xl:h-96 "
-								frameBorder="0"
-								scrolling="no"
-								allow="fullscreen;"
-								type="text/html"
-								src="https://www.youtube.com/embed/6ApGMwgCt_w?autoplay=0&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&vq=hd720&origin=https://youtubeembedcode.com"
-							></iframe>
+					<div className="
+					videoContainer
+					p-2 mx-3 my-9 flex-col rounded-md 
+					xl:flex xl:flex-row xl:m- xl:items-center 
+					xl:justify-evenly
+					">
+						<div className="wrapperwrapper xl:w-3/4 ">
+							<div className="videoWrapper  
+							xl:flex-row 
+							">
+								<iframe
+									className="h-full w-full xl:h-96 "
+									frameBorder="0"
+									scrolling="no"
+									allow="fullscreen;"
+									type="text/html"
+									src="https://www.youtube.com/embed/6ApGMwgCt_w?autoplay=0&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&vq=hd720&origin=https://youtubeembedcode.com"
+								></iframe>
+							</div>
+						</div>
+						<div className="experience__description flex-col content-center items-center xl:w-96 xl:p-6 ">
+							<h3 className="pt-5 xl:pt-0">Eye to Ear - iPad App</h3>
+							<p className="text-left">
+								Eye to Ear facilitates visually impaired museum visitors to
+								experience visual art through an interactive sonification
+								approach 
+							</p>
+							<h4 className="pt-5 xl:pt-0">Occupation: </h4>
+							<p >Sound Art Direction & Sound Design</p>
+							<h4 className="pt-5  xl:pt-0">Awards: </h4>
+							<ul className=" text-center">
+								<li>Art Directors Club of Europe</li>
+								<li>Red Dot Junior Award 2017</li>
+								<li>Creative Club Austria (Gold)</li>
+								<li>Deutscher Designer Club</li>
+								<li>IIIDAward 2017</li>
+								<li>Digital Business Trend Award 2016</li>
+								<li>Austrian Inclusion Award (Honory Mention)</li>
+							</ul>
 						</div>
 					</div>
-					<div className="experience__description flex-col content-center items-center xl:w-96 xl:p-6 ">
-						<h3 className="pt-5 xl:pt-0">Eye to Ear - iPad App</h3>
-						<p className="text-left">
-							Eye to Ear facilitates visually impaired museum visitors to
-							experience visual art through an interactive sonification
-							approach 
-						</p>
-						<h4 className="pt-5 xl:pt-0">Occupation: </h4>
-						<p >Sound Art Direction & Sound Design</p>
-						<h4 className="pt-5  xl:pt-0">Awards: </h4>
-						<ul className=" text-center">
-							<li>Art Directors Club of Europe</li>
-							<li>Red Dot Junior Award 2017</li>
-							<li>Creative Club Austria (Gold)</li>
-							<li>Deutscher Designer Club</li>
-							<li>IIIDAward 2017</li>
-							<li>Digital Business Trend Award 2016</li>
-							<li>Austrian Inclusion Award (Honory Mention)</li>
-						</ul>
-					</div>
-				</div>
 
-				<div className="
-				videoContainer
-				p-2 mx-3 my-9 flex-col rounded-md 
-				xl:flex xl:flex-row xl:m-16 xl:items-center 
-				xl:justify-evenly
-				">
-					<div className="wrapperwrapper xl:w-3/4">
-						<div className="videoWrapper  
-						xl:flex-row 
-						">
-							<iframe
-								className="h-full w-full xl:h-96 "
-								title="vimeo-player"
-								src="https://player.vimeo.com/video/127020709?h=b8366ff9e9"
-								frameBorder="0"
-								allow="fullscreen;"></iframe>
+					<div className="
+					videoContainer
+					p-2 mx-3 my-9 flex-col rounded-md 
+					xl:flex xl:flex-row xl:m- xl:items-center 
+					xl:justify-evenly
+					">
+						<div className="wrapperwrapper xl:w-3/4">
+							<div className="videoWrapper  
+							xl:flex-row 
+							">
+								<iframe
+									className="h-full w-full xl:h-96 "
+									title="vimeo-player"
+									src="https://player.vimeo.com/video/127020709?h=b8366ff9e9"
+									frameBorder="0"
+									allow="fullscreen;"></iframe>
+							</div>
+						</div>
+						<div className="experience__description flex-col content-center items-center xl:w-96 xl:p-6  ">
+							<h3 className="pt-5 xl:pt-0">Mobiglobe</h3>
+							<p className="text-left">
+								Located adjacent to the Volkswagen headquarters in Wolfsburg,
+								Germany the Autostadt serves as a communication platform. It has a
+								state-of-the-art car museum with an interactive gesturebased encyclopedia.
+							</p>
+							<h4 className="pt-5">Occupation: </h4>
+							<p className="">Sound Design</p>
+							<h4 className="pt-5">Awards: </h4>
+							<p className=" text-center ">Red Dot Award Communicationdesign</p>
 						</div>
 					</div>
-					<div className="experience__description flex-col content-center items-center xl:w-96 xl:p-6  ">
-						<h3 className="pt-5 xl:pt-0">Mobiglobe</h3>
-						<p className="text-left">
-							Located adjacent to the Volkswagen headquarters in Wolfsburg,
-							Germany the Autostadt serves as a communication platform. It has a
-							state-of-the-art car museum with an interactive gesturebased encyclopedia.
-						</p>
-						<h4 className="pt-5">Occupation: </h4>
-						<p className="">Sound Design</p>
-						<h4 className="pt-5">Awards: </h4>
-						<p className=" text-center ">Red Dot Award Communicationdesign</p>
-					</div>
-				</div>
 
 
-				<div className="
-				videoContainer
-				p-2 mx-3 my-9 flex-col rounded-md 
-				xl:flex xl:flex-row xl:m-16 xl:items-center 
-				xl:justify-evenly
-				">
-					
-					{/* //VIDEOS */}
-					<div className="wrapperwrapper xl:w-3/4">
-						<div className="videoWrapper  
-						xl:flex-row 
-						">
-							<iframe
-								className='h-full w-full xl:h-96 '
-								title="vimeo-player"
-								src="https://player.vimeo.com/video/126129405?h=dbfddfd934"
-								frameBorder="0"
-								allow="fullscreen;"
-							></iframe>
+					<div className="
+					videoContainer
+					p-2 mx-3 my-9 flex-col rounded-md 
+					xl:flex xl:flex-row xl:m- xl:items-center 
+					xl:justify-evenly
+					">
+						
+						{/* //VIDEOS */}
+						<div className="wrapperwrapper xl:w-3/4">
+							<div className="videoWrapper  
+							xl:flex-row 
+							">
+								<iframe
+									className='h-full w-full xl:h-96 '
+									title="vimeo-player"
+									src="https://player.vimeo.com/video/126129405?h=dbfddfd934"
+									frameBorder="0"
+									allow="fullscreen;"
+								></iframe>
+							</div>
+						</div>
+						<div className="experience__description flex-col content-center items-center xl:w-96 xl:p-6">
+							<h3  className="pt-5 xl:pt-0">Sasso San Gottardo</h3>
+							<p className="text-left">
+								Deep inside the swiss alps hides the once top secret Gotthard
+								fortress. Since 2012 it is open for the public and displays 7
+								rooms with informational multimedia installations created with Idee und Klang Studios treating topics like energy,
+								water, internet security and climate change
+							</p>
+							<h4 className="pt-5"> Occupation:</h4>
+							<p > Sound Design, Technical Installation</p>
 						</div>
 					</div>
-					<div className="experience__description flex-col content-center items-center xl:w-96 xl:p-6">
-						<h3  className="pt-5 xl:pt-0">Sasso San Gottardo</h3>
-						<p className="text-left">
-							Deep inside the swiss alps hides the once top secret Gotthard
-							fortress. Since 2012 it is open for the public and displays 7
-							rooms with informational multimedia installations created with Idee und Klang Studios treating topics like energy,
-							water, internet security and climate change
-						</p>
-						<h4 className="pt-5"> Occupation:</h4>
-						<p > Sound Design, Technical Installation</p>
-					</div>
-				</div>
 
-				<div className="
-				videoContainer
-				p-2 mx-3 my-9 flex-col rounded-md 
-				xl:flex xl:flex-row xl:m-16 xl:items-center 
-				xl:justify-evenly
-				">
-					
-					{/* //VIDEOS */}
-					<div className="wrapperwrapper xl:w-3/4">
-						<div className="videoWrapper  
-						xl:flex-row 
-						">
-							<iframe
-								className='h-full w-full xl:h-96 '
-								frameBorder="0"
-								allow="fullscreen;"
-								scrolling="no"
-								marginHeight="0"
-								marginWidth="0"
-								width="788.54"
-								height="443"
-								type="text/html"
-								src="https://www.youtube.com/embed/IbSHel9CRT0?autoplay=0&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&vq=hd720&origin=https://youtubeembedcode.com"
-							></iframe>
+					<div className="
+					videoContainer
+					p-2 mx-3 my-9 flex-col rounded-md 
+					xl:flex xl:flex-row xl:m- xl:items-center 
+					xl:justify-evenly
+					">
+						
+						{/* //VIDEOS */}
+						<div className="wrapperwrapper xl:w-3/4">
+							<div className="videoWrapper  
+							xl:flex-row 
+							">
+								<iframe
+									className='h-full w-full xl:h-96 '
+									frameBorder="0"
+									allow="fullscreen;"
+									scrolling="no"
+									marginHeight="0"
+									marginWidth="0"
+									width="788.54"
+									height="443"
+									type="text/html"
+									src="https://www.youtube.com/embed/IbSHel9CRT0?autoplay=0&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&vq=hd720&origin=https://youtubeembedcode.com"
+								></iframe>
+							</div>
+						</div>
+						<div className="experience__description flex-col content-center pb-5 items-center xl:w-96 xl:p-6">
+							<h3 className="pt-5 xl:pt-0">
+								Blue Cube 2.0 
+							</h3>
+							<p className="text-left">
+								In 2020 Attraktion! redesigned and upgraded the attraction park the Blue Cube at the Danfoss Universe Science Park in Denmark
+							</p>
+							<h4 className="pt-5">Occupation: </h4>
+							<p className="">Project Management</p>
+							<h4>Awards: </h4>
+							<p className="">Global Creative Technology Design Award</p>
 						</div>
 					</div>
-					<div className="experience__description flex-col content-center pb-5 items-center xl:w-96 xl:p-6">
-						<h3 className="pt-5 xl:pt-0">
-							Blue Cube 2.0 
-						</h3>
-						<p className="text-left">
-							In 2020 Attraktion! redesigned and upgraded the attraction park the Blue Cube at the Danfoss Universe Science Park in Denmark
-						</p>
-						<h4 className="pt-5">Occupation: </h4>
-						<p className="">Project Management</p>
-						<h4>Awards: </h4>
-						<p className="">Global Creative Technology Design Award</p>
-					</div>
-				</div>
 
-				<div className="
-				videoContainer
-				p-2 mx-3 my-9 flex-col rounded-md 
-				xl:flex xl:flex-row xl:m-16 xl:items-center 
-				xl:justify-evenly
-				">
-					
-					{/* //VIDEOS */}
-					<div className="wrapperwrapper xl:w-3/4">
-						<div className="videoWrapper  
-						xl:flex-row 
-						">
-							<iframe
-								className='h-full w-full xl:h-96 '
-								frameBorder="0"
-								allow="fullscreen;"
-								scrolling="no"
-								marginHeight="0"
-								marginWidth="0"
-								width="788.54"
-								height="443"
-								type="text/html"
-								src="https://www.youtube.com/embed/8eZfYiCGXQc?autoplay=0&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&vq=hd720&origin=https://youtubeembedcode.com"
-								allowFullScreen></iframe>
+					<div className="
+					videoContainer
+					p-2 mx-3 my-9 flex-col rounded-md 
+					xl:flex xl:flex-row xl:m- xl:items-center 
+					xl:justify-evenly
+					">
+						
+						{/* //VIDEOS */}
+						<div className="wrapperwrapper xl:w-3/4">
+							<div className="videoWrapper  
+							xl:flex-row 
+							">
+								<iframe
+									className='h-full w-full xl:h-96 '
+									frameBorder="0"
+									allow="fullscreen;"
+									scrolling="no"
+									marginHeight="0"
+									marginWidth="0"
+									width="788.54"
+									height="443"
+									type="text/html"
+									src="https://www.youtube.com/embed/8eZfYiCGXQc?autoplay=0&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&vq=hd720&origin=https://youtubeembedcode.com"
+									allowFullScreen></iframe>
+							</div>
+						</div>
+						<div className="experience__description flex-col content-center pb-5 items-center xl:w-96 xl:p-6">
+							<h3 className="pt-5 xl:pt-0">Attraktion! Showcase 2019/20</h3>
+							<p className="text-left "> 
+							Attraktion! creates exceptional media-based experience products such as service robots,
+							dome attractions, interactive games and exhibits 
+							</p>
+							<h4 className="pt-5">Occupation:</h4>
+							<p className=""> Project Management </p>
 						</div>
 					</div>
-					<div className="experience__description flex-col content-center pb-5 items-center xl:w-96 xl:p-6">
-						<h3 className="pt-5 xl:pt-0">Attraktion! Showcase 2019/20</h3>
-						<p className="text-left "> 
-						Attraktion! creates exceptional media-based experience products such as service robots,
-						dome attractions, interactive games and exhibits 
-						</p>
-						<h4 className="pt-5">Occupation:</h4>
-						<p className=""> Project Management </p>
-					</div>
-				</div>
+
 			</div>
+			</AnimationOnScroll>
 		</section>
 	);
 };
