ed14f5ae4212268a070ca2023dbb2bb7fb1e9346
Armand-Lluka <llukawork@gmail.com>
2022-10-07 17:50:43 +0200

    feat: added custom tailwind height

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {  
+      height: {
+        76: '18rem',
+        78: '19rem',
+        82: '22rem',
+        97: '28rem',
+        98: '31rem',
+        99: '38rem',
+        100: '40rem',
+        'fit-content': 'fit-content',
+    }},
   },
   plugins: [],
 }
