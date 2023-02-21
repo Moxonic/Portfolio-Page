0b36a70b74e9a3c0aa4ad892e1826da77ae07c3b
Daniel Soto Delgado <d.sotodelgado@outlook.com>
2023-02-21 09:39:37 +0100

    center experience text

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -20,17 +20,13 @@ const Experience = () => {
 					delay="1"
 				>
 					<h1 className=" text-center">Experience</h1>
-				</AnimationOnScroll>
-				<AnimationOnScroll
-					animateIn="animate__fadeIn"
-					duration="2"
-					delay="1"
+				
 				>
-					<p className="legend text-center p-9">
-						Welcome to my showreel! The following selection showcases a selection of my work and projects I was involved in, 
+					<p className="legend  text-center max-w-3xl m-auto p-9">
+						Welcome to my showreel! <br></br>The following selection showcases a selection of my work and projects I was involved in, 
 						so sit back, relax, and enjoy the show!
 					</p>
-					<h5 className="p-9 text-center">Enjoy!</h5>
+					
 				</AnimationOnScroll>
 			</div>
 
